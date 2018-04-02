2a93aa081016ac63837e4f01ca8a5290abe32583
bandianconde <bandianconde@yahoo.fr>
2018-04-02 20:45:15 +0200

    rien

--- src/detecte_contours_ssl.py
+++ src/detecte_contours_ssl.py
@@ -20,7 +20,8 @@ import imageio
 perimetre= lambda cnt: cv2.arcLength(cnt,True)
 def epure_contours(contours,alpha,logfile):
     """epure la liste des contours pour n'afficher que les contours les plus significatif 
-    l'unité de mesure ici est la surfce d'un contours """
+<<<<<<< HEAD
+    l'unité de mesure ici est le périmètre d'un contour """
     contours=sorted(contours ,key=lambda contour :-cv2.arcLength(contour,True))
     #apres cette étape les contours sot classés par ordre decroissant de surface 
     nb_contours=len(contours)
@@ -34,11 +35,13 @@ def epure_contours(contours,alpha,logfile):
             break
     return contours
     
-def detecte_voix_frame_1(num_frame,frameSSL,logfile):
+def detecte_voix_frame_1(num_frame,frameSSL,logfile,epure=False,debug=False):
     """ detecte la voix dans une frameSSL correspondant au fait
     qu'une seule personne parle """
+    #l'option debug permet d'afficher les images thresholdé des frames ayan plusieurs contours
     frameSSLgray= cv2.cvtColor(frameSSL, cv2.COLOR_BGR2GRAY)
     frameSSLthresh=cv2.threshold(frameSSLgray,0,255,cv2.THRESH_BINARY)[1]
+    images_path=path+"/ssl_images"
     #contours = cv2.findContours(frameSSL.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     #contours = contours[0] if imutils.is_cv2() else contours[1]
     _, contours, hierarchy=cv2.findContours(frameSSLthresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -46,12 +49,16 @@ def detecte_voix_frame_1(num_frame,frameSSL,logfile):
     centre_contours=[]
     n=len(contours)
     if n>1:
-        contours=epure_contours(contours,1/2.0,logfile)
-        m=len(contours)
-        if(m<n):
-            logfile.write("epuration de la frame n°{} on quitte de {} à {}\n".format(num_frame,n,m))
+        if epure:
+            #si l'option epuration est activé faire 
+            contours=epure_contours(contours,1/2.0,logfile)
+            m=len(contours)
+            if(m<n):
+                logfile.write("epuration de la frame n°{} on quitte de {} à {}\n".format(num_frame,n,m))
+            else:
+                logfile.write("alpha faible pas eu d'epuration pour la frame {} et il reste {} contours \n".format(num_frame,n))
         else:
-            logfile.write("alpha faible pas eu d'epuration pour la frame {} et il reste {} contours \n".format(num_frame,n))
+            logfile.write("IL y'a plusieurs contours {} et il n'y a pas eu d'épuration pour la frame {}".format(n,num_frame))
     for contour in contours:
         nb_contours+=1
         cv2.drawContours(frameSSLthresh, contour, -1, (25,25,127),1)
@@ -61,8 +68,11 @@ def detecte_voix_frame_1(num_frame,frameSSL,logfile):
         cY=int(M["m01"] / M["m00"])
         centre_contours.append([cX,cY])
         #print("Le centre du cercle se trouve a ",cX,cY)
-        cv2.circle(frameSSLthresh, (cX,cY), 2,[0,0,0], 5)
-
+        cv2.circle(frameSSLthresh, (cX,cY), 5,[120,120,120], 5)
+    if n>1 and debug:
+        #On verifie que y'a plusieurs contours et que le mode debug est activé
+        frameSSLtresh=cv2.threshold(frameSSLgray,0,255,cv2.THRESH_BINARY)[1]
+        imageio.imwrite(images_path+'/ssl_image_{}_thresh.jpg'.format(num_frame),frameSSLtresh)
     #cv2.imshow('Observations',frameSSLthresh)
     #cv2.waitKey(2000)
     if n==0:
@@ -88,11 +98,10 @@ def detecte_voix_1(path):
         frameSSL=cv2.resize(frameSSL,(frameShape[1],frameShape[0]), interpolation=cv2.INTER_CUBIC)
             #modifier cette partie pour le cas ou plusieurs personnes parle donc plusieurs intensité lumineuse 
         #y,x=detection_max_intensite(frameSSL)[0:2]
-        centre_contours=detecte_voix_frame_1(num_frame,frameSSL,logfile)
+
+        centre_contours=detecte_voix_frame_1(num_frame,frameSSL,logfile,epure=False)
         n=len(centre_contours)
         for i in range(n):
-            if n>1:
-                detection_ssl.write("detections de plusieurs contours")
             y,x=centre_contours[i]
             #print("Le maximum d'intensité est a ",x,y)
             detection_ssl.write("{}".format(num_frame))
